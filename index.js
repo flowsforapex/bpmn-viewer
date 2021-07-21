86c64309bbc9a7c2a2cb034ba50f8079060d8d56
damthor <dennis.amthor@mt-ag.com>
2021-07-21 14:16:45 +0200

    Display overlay on hover event

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import BpmnJS from "bpmn-js/lib/Viewer";
 import BpmnJSNavigated from "bpmn-js/lib/NavigatedViewer"
 import spViewModule from "./lib/spViewModule";
 import styleModule from "./lib/styleModule";
+import hoverModule from "./lib/hoverModule";
 
 var bpmnViewer = {
   Viewer: BpmnJS,
   NavigatedViewer: BpmnJSNavigated,
   customModules: { 
     spViewModule,
-    styleModule
+    styleModule,
+    hoverModule
   }
 };
 
