0c0f3724f1bc399df594149070021aa3061f0426
Dennis Amthor <79202384+damthor@users.noreply.github.com>
2022-04-04 11:21:00 +0200

    Removed unsupported spViewModule

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import BpmnJSNavigated from 'bpmn-js/lib/NavigatedViewer';
 import BpmnJS from 'bpmn-js/lib/Viewer';
-import spViewModule from './lib/spViewModule';
 import styleModule from './lib/styleModule';
 import subProcessModule from './lib/subProcessModule';
 
@@ -8,7 +7,6 @@ var bpmnViewer = {
   Viewer: BpmnJS,
   NavigatedViewer: BpmnJSNavigated,
   customModules: {
-    spViewModule,
     styleModule,
     subProcessModule,
   },
