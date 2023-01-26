6029c3d011b58521f6413ce75fe6a50451a60cd6
Dennis Amthor <79202384+damthor@users.noreply.github.com>
2023-01-26 15:17:34 +0100

    Finalized breadcrumb rework (fixed visibility/positioning, refactoring)

--- CustomViewer.js
+++ CustomViewer.js
@@ -1,20 +1,8 @@
 import Viewer from 'bpmn-js/lib/Viewer';
 import MoveCanvasModule from 'diagram-js/lib/navigation/movecanvas';
 import ZoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
+import customDrilldownModule from './lib/customDrilldown';
 import styleModule from './lib/styleModule';
-// import subProcessModule from './lib/subProcessModule';
-import DrilldownModdule from './lib/callActivityDrilldown';
-
-// var bpmnViewer = {
-//   Viewer: Viewer,
-//   NavigatedViewer: BpmnJSNavigated,
-//   customModules: {
-//     styleModule,
-//     subProcessModule,
-//   },
-// };
-
-// export default bpmnViewer;
 
 export default class CustomViewer extends Viewer {
 }
@@ -22,12 +10,11 @@ export default class CustomViewer extends Viewer {
 CustomViewer.prototype._customModules = [
   ZoomScrollModule,
   MoveCanvasModule,
-  DrilldownModdule,
-  styleModule
+  styleModule,
+  customDrilldownModule
 ];
 
 CustomViewer.prototype._modules = [].concat(
-  // Viewer.prototype._modules.filter(f => !f.drilldownBreadcrumbs),
-  Viewer.prototype._modules,
+  Viewer.prototype._modules.filter(f => !f.drilldownBreadcrumbs),
   CustomViewer.prototype._customModules
 );
